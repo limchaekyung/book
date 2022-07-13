e028dcd5c9ff2821190184646a1526e988ceac23
limchaekyung <corud505@naver.com>
2022-07-13 23:22:12 +0900

    book form

--- routes/books.js
+++ routes/books.js
@@ -1,20 +1,30 @@
 const express = require('express')
 const router = express.Router()
-const Book = require('../models/author')
+const Author = require('../models/author')
+const Book = require('../models/book')
 
 // All Books Route
 router.get('/', async (req, res) => {
-
+    res.send('All Books')
 })
 
 // New Book Route
-router.get('/new', (req, res) => {
-
+router.get('/new', async (req, res) => {
+    try {
+        const authors = await Author.find({})
+        const book = new Book()
+        res.render('books/new', {
+            authors: authors,
+            book: book
+        })
+    } catch {
+        res.redirect('/')
+    }
 })
 
 // Create Book Route
 router.post('/', async (req, res) => {
-
+    res.sned('Create Book')
 })
 
 module.exports = router
