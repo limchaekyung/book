37925e65597534c1e46b043ba9d66cb6a40d0797
limchaekyung <corud505@naver.com>
2022-07-17 23:57:58 +0900

    upload2

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -3,4 +3,11 @@ FilePond.registerPlugin(
     FilePondPluginImageResize,
     FilePondPluginImageEncode,
 )
+
+FilePond.setOptions({
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150
+})
+
 FilePond.parse(document.body);
